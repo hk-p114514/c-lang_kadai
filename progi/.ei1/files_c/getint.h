74f085707944e3b045feb3bc6077e6126298aea7
hk-p114514 <hukada.gogotea@gmail.com>
2021-06-02 10:47:55 +0900

    授業分を追加

--- progi/.ei1/files_c/getint.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int getint(char message[99]) {
-    int elem;
-    printf("%s", message);
-    scanf("%d", &elem);
-    return elem;
-}
--- progi/.ei1/files_c/kadai/prog03/test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-main()
-{
-    printf("%d\n", '~' - ' ' + 1);
-
-    return (0);
-}
