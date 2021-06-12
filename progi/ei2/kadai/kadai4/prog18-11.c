3bfd5baa1433600f8750a758b61b8e762f5b31e1
hk-p114514 <hukada.gogotea@gmail.com>
2021-06-12 20:48:34 +0900

    18-11

--- progi/ei2/kadai/kadai4/prog18-11.c
+++ progi/ei2/kadai/kadai4/prog18-11.c
@@ -13,13 +13,14 @@ Fraction frcSub(Fraction x, Fraction y);
 Fraction frcMul(Fraction x, Fraction y);
 Fraction frcDiv(Fraction x, Fraction y);
 Fraction frcReduction(Fraction x);
+Fraction frcCommonDenominator();
 int frcGetNumerator(Fraction x);
 int frcGetDenominator(Fraction x);
 void frcPrint(Fraction x);
 void frcPrintOperation(char op, Fraction x, Fraction y);
 // 名前変えました
 int getGcm(int m, int n);
-int getGcd(int m, int n);
+int getLcm(int m, int n);
 
 int main() {
 	Fraction a, b, c;
@@ -33,20 +34,16 @@ int main() {
 	b = getFraction();
 
 	// 分数a, bに対して、四則演算を実行する
-	// for (i = 0; op[ i ] != '\0'; i++) {
-	// 	frcPrintOperation(op[ i ], a, b); // 四則演算の結果表示
-	// }
-
-	frcPrintOperation(op[ 0 ], a, b); // 四則演算の結果表示
-	// frcPrintOperation(op[ 1 ], a, b); // 四則演算の結果表示
-	// frcPrintOperation(op[ 2 ], a, b); // 四則演算の結果表示
-	// frcPrintOperation(op[ 3 ], a, b); // 四則演算の結果表示
+	for (i = 0; op[ i ] != '\0'; i++) {
+		frcPrintOperation(op[ i ], a, b); // 四則演算の結果表示
+	}
 
 	putchar('\n'); // 見やすくするために改行
 
 	return (0);
 }
 
+/*========================================*/
 // 分数xと分数yに、2項演算(x op y)を実行した結果を表示する
 //   [引　数] op   : 2項演算子（+, -, *, /)
 //            x, y : 演算対象となる分数
@@ -82,6 +79,7 @@ void frcPrintOperation(char op, Fraction x, Fraction y) {
 	return;
 }
 
+/*========================================*/
 // Fraction型で表される分数の分子の値を返す
 // [引　数] Fraction型で表現される分数
 // [戻り値] 分子の値
@@ -89,6 +87,7 @@ int frcGetNumerator(Fraction x) {
 	return (x.numerator);
 }
 
+/*========================================*/
 // Fraction型で表される分数の分母の値を返す
 // [引　数] Fraction型で表現される分数
 // [戻り値] 分母の値
@@ -96,6 +95,7 @@ int frcGetDenominator(Fraction x) {
 	return (x.denominator);
 }
 
+/*========================================*/
 // キーボードより、a/bの形で分数を入力する
 //   [引　数] なし
 //   [戻り値] Fraction型で表現される分数
@@ -108,6 +108,7 @@ Fraction getFraction(void) {
 	return (input);
 }
 
+/*========================================*/
 // 分子と分母の値からFraction型で表される分数を返す
 //   [引　数] numerator   : 分子の値
 //            denominator : 分母の値
@@ -121,49 +122,54 @@ Fraction frcCreate(int numerator, int denominator) {
 	return (created);
 }
 
+/*========================================*/
 // 分数xとyの加算結果を返す
 //   [引　数] x, y : 加算対象となる分数
 //   [戻り値] x + y を計算して得られる分数
 Fraction frcAdd(Fraction x, Fraction y) {
+	// 各分数の分母、分子の値を読み込む
 	Fraction added;
+	// 各分数の分母、分子の値を読み込む
 	int numeratorX = frcGetNumerator(x);
 	int denominatorX = frcGetDenominator(x);
 
 	int numeratorY = frcGetNumerator(y);
 	int denominatorY = frcGetDenominator(y);
 
-	// 足し算する2つの分数の分母が違う場合、通分して分母を合わせる
-	if (denominatorX != denominatorY) {
-		// 二つの分母の値の最小公倍数を求める
-		int gcd = getGcd(denominatorX, denominatorY);
-		// 各分母がどれくらい変化したかを求める
-		int changeX = gcd / denominatorX;
-		int changeY = gcd / denominatorY;
+	// 二つの分母の値の最小公倍数を求める
+	int lcm = getLcm(denominatorX, denominatorY);
+	// 各分母がどれくらい変化したかを求める
+	int changeX = lcm / denominatorX;
+	int changeY = lcm / denominatorY;
+	// 分子の値を分母に合わせる
+	numeratorX *= changeX;
+	numeratorY *= changeY;
+	// 加算結果の分数の分母を求めた最小公倍数にする
+	added.denominator = lcm;
 
-		// 分子の値を分母に合わせる
-		numeratorX *= changeX;
-		numeratorY *= changeY;
-
-		// 二つの分数の分母を求めた最小公倍数に直す
-		denominatorX = gcd;
-		denominatorY = gcd;
-	}
+	added.numerator = numeratorX + numeratorY;
 
 	return (added);
 }
 
+/*========================================*/
 // 分数xとyの減算結果を返す
 //   [引　数] x, y : 減算対象となる分数
 //   [戻り値] x - y を計算して得られる分数
 Fraction frcSub(Fraction x, Fraction y) {
 	Fraction substracted;
+	// 引き算は足し算の逆（符号が）なので、frcAddが使える
 
-	substracted.numerator = frcGetNumerator(x) - frcGetNumerator(y);
-	substracted.denominator = frcGetDenominator(x) - frcGetDenominator(y);
+	// 引く方の分子をマイナスにしてfrcAddにわたす。
+	int numeratorY = frcGetNumerator(y);
+	int denominatorY = frcGetDenominator(y);
+
+	substracted = frcAdd(x, frcCreate(-numeratorY, denominatorY));
 
 	return (substracted);
 }
 
+/*========================================*/
 // 分数xとyの乗算結果を返す
 //   [引　数] x, y : 乗算対象となる分数
 //   [戻り値] x * y を計算して得られる分数
@@ -177,25 +183,38 @@ Fraction frcMul(Fraction x, Fraction y) {
 	return (multiplied);
 }
 
+/*========================================*/
 // 分数xとyの除算結果を返す
 //   [引　数] x, y : 除算対象となる分数
 //   [戻り値] x / y を計算して得られる分数
 Fraction frcDiv(Fraction x, Fraction y) {
-
 	Fraction isDivision;
 
-	isDivision.numerator = frcGetNumerator(x) - frcGetNumerator(y);
-	isDivision.denominator = frcGetDenominator(x) - frcGetDenominator(y);
+	isDivision.numerator = x.numerator * y.denominator;
+	isDivision.denominator = x.denominator * y.numerator;
 
 	return (isDivision);
 }
 
+/*========================================*/
 // 分数xの既約分数を返す
 //   [引　数] x : 約分対象となる分数
 //   [戻り値] xの既約分数
 Fraction frcReduction(Fraction x) {
+	int numerator = frcGetNumerator(x);
+	int denominator = frcGetDenominator(x);
+	// 分母と分子の最大公約数を求める
+	int gcm = getGcm(numerator, denominator);
+	// 分母と分子を最大公約数で割る
+	numerator /= gcm;
+	denominator /= gcm;
+
+	Fraction result = frcCreate(numerator, denominator);
+
+	return (result);
 }
 
+/*========================================*/
 // mとnの最大公約数を正数で返す
 //   [引　数] m, n : 最大公約数を求める対象となる整数
 //   [戻り値] mとmの最大公約数
@@ -215,16 +234,18 @@ int getGcm(int m, int n) {
 	return (n);
 }
 
+/*========================================*/
 // mとnの最小公倍数を整数で返す
 //   [引　数] m, n : 最小公倍数を求める対象となる整数
 //   [戻り値] mとmの最小公倍数
-int getGcd(int n, int m) {
+int getLcm(int n, int m) {
 	int x = n * m;
-	int gcm = getGcd(n, m);
+	int gcm = getGcm(n, m);
 
 	return (x / gcm);
 }
 
+/*========================================*/
 // 分数xを a/b の形で表示する（分数の値が負になるとき、符号は分子につける）
 //   [引　数] x : 表示する分数
 void frcPrint(Fraction x) {
@@ -233,17 +254,24 @@ void frcPrint(Fraction x) {
 	n = frcGetNumerator(x);
 	d = frcGetDenominator(x);
 
-	if (d < 0) {
+	if (n * d < 0) {
+		// n * dがマイナスの時 = nかdのどちらかがマイナスの時
+		// 分子にマイナスをつけ直し、分母の値はその絶対値にする
 		n = -abs(n);
 		d = abs(d);
 	} else {
 		n = abs(n);
 		d = abs(d);
 	}
+
 	if (n == 0 || d == 1) {
-		printf("%d\n", n);
+		// 分子が0か分母が１の時は(整数になる場合)
+		// 分子の値を出力する
+		printf("%d", n);
 	} else {
-		printf("%d/%d\n", n, d);
+		// そうでない場合、
+		// n/dの形式で出力する
+		printf("%d/%d", n, d);
 	}
 
 	return;
