5dcfb37be8b98fac244764e035fb9cf60dd5477e
hk-p114514 <hukada.gogotea@gmail.com>
2021-06-16 18:44:39 +0900

    19入った

--- progi/ei2/kadai/kadai5/sample.c
+++ progi/ei2/kadai/kadai5/sample.c
@@ -1,6 +1,4 @@
-#include <math.h>
 #include <stdio.h>
-#define SQRT(x) ((x) * (x))
 
 typedef struct {
 	double x; // ｘ座標
@@ -23,8 +21,9 @@ double rctArea(Rect rct);
 double rctPerim(Rect rct);
 double rctWidth(Rect rct);
 double rctHeight(Rect rct);
-double rctDialoglen(Rect rct);
 Point rctTopLeftCorner(Rect rct);
+Point rctPoint1(Rect rct);
+Point rctPoint2(Rect rct);
 Rect rctCreate1(Point p1, Point p2);
 Rect rctCreate2(Point p, double width, double height);
 
@@ -112,7 +111,7 @@ double rctPerim(Rect rct) {
 // 第一引数: 幅を求めたいRect型の値
 // 返り値  : 求めた幅
 double rctWidth(Rect rct) {
-	double width = ptGetX(rct.p1) - ptGetX(rct.p2);
+	double width = ptGetX(rctPoint1(rct)) - ptGetX(rctPoint2(rct));
 	if (width < 0) {
 		width = -width;
 	}
@@ -128,7 +127,7 @@ double rctWidth(Rect rct) {
 // 第一引数: 高さを求めたいRect型の値
 // 返り値  : 求めた高さ
 double rctHeight(Rect rct) {
-	double height = ptGetX(rct.p1) - ptGetX(rct.p2);
+	double height = ptGetX(rctPoint1(rct)) - ptGetX(rctPoint2(rct));
 	if (height < 0) {
 		height = -height;
 	}
@@ -145,11 +144,11 @@ double rctHeight(Rect rct) {
 // 返り値  : 調べた左上隅の点
 Point rctTopLeftCorner(Rect rct) {
 	double x1, x2, y1, y2;
-	x1 = ptGetX(rct.p1);
-	x2 = ptGetX(rct.p2);
+	x1 = ptGetX(rctPoint1(rct));
+	x2 = ptGetX(rctPoint2(rct));
 
-	y1 = ptGetY(rct.p1);
-	y2 = ptGetY(rct.p2);
+	y1 = ptGetY(rctPoint1(rct));
+	y2 = ptGetY(rctPoint2(rct));
 	double top, left;
 
 	if (x1 < x2) {
@@ -161,59 +160,22 @@ Point rctTopLeftCorner(Rect rct) {
 
 /*========================================*/
 
-/* rctCreate1()
-    概要:対角線を表す2点p1,p2から、長方形を生成する
+/* rctPoint1()
+    概要:長方形rctの対角線の端点の内1つ目を返す
 */
-// 第1数: 対角線を表す点
-// 第2引数: 対角線を表す点
-// 返り値  : 生成したRect型の長方形
-Rect rctCreate1(Point p1, Point p2) {
-	Rect create;
-	create.p1 = p1;
-	create.p2 = p2;
-
-	return (create);
+// 第一引数: Rect型の長方形の値
+// 返り値  : 求めたPoint型の値
+Point rctPoint1(Rect rct) {
+	return (rct.p1);
 }
 
 /*========================================*/
 
-/* rctCreate2()
-    概要:左上の1点pと、幅 width及び高さ height の長さから、長方形を生成する
+/* rctPoint2()
+    概要:長方形rctの対角線の端点の内2つ目を返す
 */
-// 第1引数: 左上の点p
-// 第2引数: 横幅
-// 第3引数: 高さ
-// 返り値  : 生成した長方形
-Rect rctCreate2(Point p, double width, double height) {
-	Rect create;
-	create.p1 = p;
-	Point p2 = ptCreate(ptGetX(p) + width, ptGetY(p) + height);
-	create.p2 = p2;
-
-	return (create);
-}
-
-/*========================================*/
-
-/* rctDialoglen()
-    概要:長方形rctの対角線の長さを返す
-*/
-// 第1引数: 対角線の長さを求めたいRect型の値
-// 返り値  : 求めた対角線の長さ
-double rctDialoglen(Rect rct) {
-	Point topLeft = rctTopLeftCorner(rct);
-	// 左上隅の点の各座標
-	double x1 = ptGetX(topLeft);
-	double y1 = ptGetY(topLeft);
-
-	// 右下の点の各座標
-	double x2 = x1 + rctWidth(rct);
-	double y2 = y1 + rctHeight(rct);
-
-	/*=============================
-	    対角線を求める式は
-	    r^2 = x^2 + y^2
-	==============================*/
-
-	double dialogLen = sqrt()
+// 第一引数: Rect型の長方形の値
+// 返り値  : 求めたPoint型の値
+Point rctPoint2(Rect rct) {
+	return (rct.p2);
 }
