85c88aff2d37c22fe5e726a23990e1fb9d61f5cf
hk-p114514 <hukada.gogotea@gmail.com>
2021-06-25 07:06:40 +0900

    18-05わからｎ

--- c-master/c-master6/toi18/toi18-05.c
+++ c-master/c-master6/toi18/toi18-05.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-void removeLineBreaks(char name[], char removed[]);
+int removeLineBreaks(char *name, char *removed);
 
 int main() {
 	char name[ 256 ], *mei, *sei;
@@ -10,17 +10,20 @@ int main() {
 	printf("「名/姓」からなる文字列を入力してください\n");
 	printf("［例 Tarou/Hamakou ］: ");
 	mei = fgets(name, sizeof(name), stdin);
-	// - mei => aaa/bbb
 
 	// 2. 最後の改行文字を取り除く
 	char removed[ 256 ];
-	removeLineBreaks(name, removed);
+	int n; // 最後の改行文字を含まない文字数
+	n = removeLineBreaks(name, removed);
 
 	// 3. スラッシュの位置を探す
 	char *slashPosition = strchr(removed, '/');
+	// *slashPosition => '/'
 
 	// 4. スラッシュの位置を\0に置き換える
 	*(slashPosition) = '\0';
+	mei = removed;
+	printf("removed = %s\n", removed);
 
 	sei = slashPosition;
 	// - sei => \0
@@ -28,18 +31,20 @@ int main() {
 	if (sei == NULL) {
 		printf("入力した文字列に\"/\"が含まれていません。\n");
 	} else {
-		*sei = *slashPosition;
-		sei = slashPosition;
+		// *sei = ;
+		// sei = ;
+
 		printf("名: %s, 姓: %s\n", mei, sei);
 	}
 
 	return (0);
 }
 
-void removeLineBreaks(char name[], char removed[]) {
-	for (int i = 0; name[ i ] != '\n'; i++) {
+int removeLineBreaks(char *name, char *removed) {
+	int i;
+	for (i = 0; name[ i + 1 ] != '\0'; i++) {
 		removed[ i ] = name[ i ];
 	}
 
-	return;
+	return (i + 1);
 }
