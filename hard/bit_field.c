b21f3546d42498f624880e7c6790309677fdebdb
hk-p114514 <hukada.gogotea@gmail.com>
2021-07-12 12:19:43 +0900

    できてるかも

--- hard/bit_field.c
+++ hard/bit_field.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 union sensor_def {
-	unsigned int byte;
-	struct bit_def {
+	unsigned int word;
+	struct bit {
 		unsigned int FL : 3;
 		unsigned int CT : 3;
 		unsigned int FR : 3;
 	} bit;
-} sensor;
+};
 
 int main() {
+	union sensor_def sensor;
 
 	return (0);
 }
