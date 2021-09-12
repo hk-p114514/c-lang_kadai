702d7b18e89fe0b091f042b29b870d5298f5910f
hk-p114514 <hukada.gogotea@gmail.com>
2021-09-12 17:17:22 +0900

    20-3だめっぽおい

--- c-master/c-master7/toi20/toi20-03.c
+++ c-master/c-master7/toi20/toi20-03.c
@@ -11,6 +11,11 @@ int main() {
 	char **p;
 	p = (char **)malloc(sizeof(char) * n);
 
+	if (p == NULL) {
+		printf("メモリを確保できませんでした。\n");
+		return (1);
+	}
+
 	for (int i = 0; i < n; i++) {
 		// 文字列の入力
 		printf("文字列[%d] : ", i);
@@ -22,6 +27,16 @@ int main() {
 
 		// 格納
 		p[ i ] = (char *)malloc(sizeof(char) * len);
+
+		if (p[ i ] == NULL) {
+			for (int j = 0; j < i; j++) {
+				free(p[ i ]);
+			}
+			free(p);
+			printf("メモリを確保できませんでした。\n");
+			return (1);
+		}
+
 		for (int j = 0; j < len; j++) {
 			p[ i ][ j ] = s[ j ];
 		}
@@ -33,8 +48,10 @@ int main() {
 		putchar('\n');
 	}
 
+	printf("============================\n");
+
 	for (int i = 0; i < n; i++) {
-		free(p[ i ]);
+		free(&p[ i ]);
 	}
 
 	free(p);
