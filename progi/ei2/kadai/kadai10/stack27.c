55427fe903410b07ad60a74ac76fa6a2a1c91f08
hk-p114514 <hukada.gogotea@gmail.com>
2021-10-30 23:33:36 +0900

    スタックできた

--- progi/ei2/kadai/kadai10/stack27.c
+++ progi/ei2/kadai/kadai10/stack27.c
@@ -1,38 +1,40 @@
 #include "stack27.h"
 #include <stdio.h>
 
-void initStack(Stack *stack);
-int push(Stack *stack, int data);
-int pop(Stack *stack, int *data);
-void freeStack(Stack *stack);
-
 // スタックの初期化
 // 引数: 操作対象のスタックを指すポインタ
 // 戻り値: なし
-void initStack(Stack *stack) {
-	*stack->head = getEmptyList();
+void initStack(Stack *s) {
+	*s->head = getEmptyList();
 	return;
 }
 
 // スタックにデータを追加
 // 引数: 操作対象のスタックを指すポインタ、追加するデータ
 // 戻り値: 追加に成功した場合は1、追加に失敗した場合は0
-int push(Stack *stack, int data) {
-	return (insertHead(stack->head, data));
+int push(Stack *s, int data) {
+	return (insertHead(s->head, data));
 }
 
 // スタックからデータを取り出す
 // 引数: 操作対象のスタックを指すポインタ、取り出したデータを格納する変数へのポインタ
 // 戻り地: 取り出しに成功した場合は1、取り出しに失敗した場合は0
-int pop(Stack *stack, int *data) {
-	*data = getCellData(*stack->head);
-	return (removeHead(stack->head));
+// Summary: Extracting data from the stack
+// Arguments: A pointer to the stack, a pointer to the variable to store the extracted
+// data Return: 1 if the extraction is successful, 0 if the extraction is failed
+int pop(Stack *s, int *data) {
+	if (isEmptyList(*s->head)) {
+		return 0;
+	}
+	*data = getCellData(*s->head);
+	removeHead(s->head);
+	return (1);
 }
 
 // スタックを解放する
 // 引数: 操作対象のスタックを指すポインタ
 // 戻り値: なし
-void freeStack(Stack *stack) {
-	removeList(stack->head);
+void freeStack(Stack *s) {
+	removeList(s->head);
 	return;
 }
