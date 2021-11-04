0581ade2dddb4968633caa8787fd9e08f51490d2
hk-p114514 <hukada.gogotea@gmail.com>
2021-11-04 10:30:20 +0900

    再帰授業

---
+++ progi/ei2/kadai/kadai10/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "hamako.h"
+
+int main() {
+  int n = getint("getint : ");
+  printf("n = %d\n", n);
+
+  return(0);
+}
--- progi/ei2/kadai/kadai10/prog27-06.c
+++ progi/ei2/kadai/kadai10/prog27-06.c
@@ -10,7 +10,7 @@ int main() {
 
 	num = getint("キューに格納するデータ数：");
 
-	for (i = 0; i < num; i++) { // キューに指定した個数のデータを格納する
+	for (i = 0; i < num; i++) {           // キューに指定した個数のデータを格納する
 		data = getint("enQueue data : "); // データ入力
 		state = enQueue(&q, data);        // 入力データを格納
 	}
@@ -19,7 +19,7 @@ int main() {
 
 	state = deQueue(&q, &data); // データの取り出し
 	putchar('\n');
-	while (state == 1) { // キューが空になるまでデータを取り出す
+	while (state == 1) {                     // キューが空になるまでデータを取り出す
 		printf("deQueue data : %d\n", data); // 取り出したデータの表示
 		state = deQueue(&q, &data);          // データの取り出し
 	}
