d96fa8f396fd2866ebc3d04ae414f243fd00e608
hk-p114514 <hukada.gogotea@gmail.com>
2021-11-12 00:03:30 +0900

    一回目提出

--- progi/ei2/kadai/kadai11/prog28-10.c
+++ progi/ei2/kadai/kadai11/prog28-10.c
@@ -28,7 +28,19 @@ int partitionNumber(int n) {
 // nをa以下の数の和で表す
 int partitionNumberRec(int n, int a) {
 	/*
-	 *基底条件: n = 1のときは1
-	 *
-	 */
+	    基底条件:
+	      a = 1 (1以下の自然数の和で表す = 全て1)のとき = それ以上分割できないとき= 1
+	      n < 0 or a <= 0 (範囲外)のとき、0
+	*/
+	if (a == 1) {
+		return (1);
+	} else if (n < 0 || a <= 0) {
+		return (0);
+	}
+
+	/*
+	    p(n,a)をnを自然数aで始まるように分割するときの分割数とすると、
+	    p(n,a) = p(n-a,a) + p(n,a-1)といえる。
+	*/
+	return (partitionNumberRec(n - a, a) + partitionNumberRec(n, a - 1));
 }
