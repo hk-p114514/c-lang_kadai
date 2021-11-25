7992595aa7f1cd698e0463afc5ab1762390c8c86
hk-p114514 <hukada.gogotea@gmail.com>
2021-11-25 10:30:57 +0900

    30start

--- progi/ei2/kadai/kadai12/cntHanoi.c
+++ progi/ei2/kadai/kadai12/cntHanoi.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-long long cntHanoi(int n);
+long long cntHanoi(long long n);
 
-int main(int argc, char *argv[])
-{
-  int n;
+int main(int argc, char *argv[]) {
+	long long n;
 
-  if (argc != 2) {
-    fprintf(stderr, "Usage: %s <number of disk>\n", argv[0]);
-    exit(1);
-  }
-  sscanf(argv[1], "%d", &n);
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <number of disk>\n", argv[ 0 ]);
+		exit(1);
+	}
+	sscanf(argv[ 1 ], "%d", &n);
 
-  printf("%d枚を移動する手順: %lld回\n", n, cntHanoi(n));
+	printf("%d枚を移動する手順: %lld回\n", n, cntHanoi(n));
 
-  return(0);
+	return (0);
 }
 
-long long cntHanoi(int n)
-{
-  if(     ) {
-    return(                       );
-  } else {
-    return(0);
-  }
+long long cntHanoi(long long n) {
+	if (n > 0) {
+		return (2 * cntHanoi(n - 1) + 1);
+	} else {
+		return (0);
+	}
 }
--- progi/ei2/kadai/kadai12/mvHanoi.c
+++ progi/ei2/kadai/kadai12/mvHanoi.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void mvHanoi(int n, char from, char work, char to);
 
-int main(int argc, char *argv[])
-{
-  int n;
+int main(int argc, char *argv[]) {
+	int n;
 
-  if (argc != 2) {
-    fprintf(stderr, "Usage: %s <number of disk>\n", argv[0]);
-    exit(1);
-  } 
-  sscanf(argv[1], "%d", &n);
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <number of disk>\n", argv[ 0 ]);
+		exit(1);
+	}
+	sscanf(argv[ 1 ], "%d", &n);
 
-  mvHanoi(n, 'A',    ,    );
+	mvHanoi(n, 'A', 'B', 'C');
 
-  return(0);
+	return (0);
 }
 
-void mvHanoi(int n, char from, char work, char to)
-{
-  if (      ) {
-                                   ;
-    printf("%c -> %c\n", from, to);
-                                   ;
-  }
-  return;
+void mvHanoi(int n, char from, char work, char to) {
+	if (n >= 1) {
+		mvHanoi(n - 1, from, to, work);
+		printf("%c -> %c\n", from, to);
+		mvHanoi(n - 1, work, from, to);
+	}
+	return;
 }
