c5afc34389670a09d0a92ee34933d2bc2df7ee8c
hk-p114514 <hukada.gogotea@gmail.com>
2021-11-26 18:48:09 +0900

    row col 見てから四方に散る

--- progi/ei2/kadai/kadai12/prog29-01.c
+++ progi/ei2/kadai/kadai12/prog29-01.c
@@ -22,14 +22,32 @@ main(int argc, char *argv[]) {
 
 // 文字列wordの右からn文字分を左回りで1文字回転させる
 void rotate(char *word, int n) {
-	int len = strlen(word);
+	int len = strlen(word) - 1;
 	// 文字列の最後尾の文字を保持する
-	char last = word[ len - 1 ];
+	char last = word[ len ];
 	// 文字列の右からn文字の内、最後尾を除いたn - 1文字を右にずらす
+	for (int i = 0; i < n; i++) {
+		int p = len - i;
+		word[ p ] = word[ p - 1 ];
+	}
 	// 右からn番目に最後尾だった文字を格納する
+	word[ len - (n - 1) ] = last;
 
 	return;
 }
 
 void anagram(char *word, int n) {
+	// 基底条件：n = 1の時、wordそのもの
+	if (n <= 1) {
+		printf("%s\n", word);
+		return;
+	} else {
+		// wordの各文字全てが先頭になった時の場合を出す
+		for (int i = 0; i < n; i++) {
+			anagram(word, n - 1);
+			rotate(word, n);
+		}
+	}
+
+	return;
 }
--- progi/ei2/kadai/kadai12/prog29-03.c
+++ progi/ei2/kadai/kadai12/prog29-03.c
@@ -3,38 +3,49 @@
 #define W 21
 #define H 20
 
-int tileCount(char room[H][W], int row, int col, int h, int w);
+int tileCount(char room[ H ][ W ], int row, int col, int h, int w);
 
-int main()
-{
-  char room[H][W];
-  int h;    // 部屋の行方向のタイルの枚数
-  int w;    // 部屋の列方向のタイルの枚数
-  int row;  // 最初に人が立っている行位置
-  int col;  // 最初に人が立っている列位置
-  int i, j;
+int main() {
+	char room[ H ][ W ];
+	int h;   // 部屋の行方向のタイルの枚数
+	int w;   // 部屋の列方向のタイルの枚数
+	int row; // 最初に人が立っている行位置
+	int col; // 最初に人が立っている列位置
+	int i, j;
 
-  // 部屋の情報を、配列room[]に文字列として読み込む
-  scanf("%d%d", &w, &h);
-  while (h != 0 && w != 0) {
-    for (i = 0; i < h; i++) {
-      scanf("%s", room[i]);
-      for (j = 0; j < w; j++) {
-        if (room[i][j] == '@') {
-          row = i;  // 最初に人が立っている行位置
-          col = j;  // 最初に人が立っている列位置
-        }
-      }
-    }
-    printf("%d\n", tileCount(room, row, col, h, w));
+	// 部屋の情報を、配列room[]に文字列として読み込む
+	scanf("%d%d", &w, &h);
+	while (h != 0 && w != 0) {
+		for (i = 0; i < h; i++) {
+			scanf("%s", room[ i ]);
+			for (j = 0; j < w; j++) {
+				if (room[ i ][ j ] == '@') {
+					row = i; // 最初に人が立っている行位置
+					col = j; // 最初に人が立っている列位置
+				}
+			}
+		}
+		printf("%d\n", tileCount(room, row, col, h, w));
 
-    scanf("%d%d", &w, &h);
-  }
-  
-  return(0);
+		scanf("%d%d", &w, &h);
+	}
+
+	return (0);
 }
 
-int tileCount(char room[H][W], int row, int col, int h, int w)
-{
+int tileCount(char room[ H ][ W ], int row, int col, int h, int w) {
+	// 基底条件：もうどこにも行けない -> 1
+	char red = '#';
+	char floor = '.';
+	int count = 1;
 
+	int up = row - 1;
+	int down = row + 1;
+	int left = col - 1;
+	int right = col + 1;
+
+	if ((row < 0 || row >= h || col < 0 || col >= w)) {
+	}
+
+	return (count);
 }
