6f793b814f5d3464e0478ec6fa68ef118fbef9bf
hk-p114514 <hukada.gogotea@gmail.com>
2021-11-27 19:16:31 +0900

    30-02完了

--- progi/ei2/kadai/kadai12/prog30-02.c
+++ progi/ei2/kadai/kadai12/prog30-02.c
@@ -1,8 +1,6 @@
-//
-//  分割数（整数ｎを正の数の和で表す）
-//
 #include <stdio.h>
 #include <stdlib.h>
+#define ARR_SIZE (100)
 
 int partitionNumber(int n);
 int partitionNumberRec(int n, int a);
@@ -25,33 +23,34 @@ int partitionNumber(int n) {
 	return (partitionNumberRec(n, n)); // nをn以下の数の和で表す
 }
 
-// nをa以下の数の和で、メモ化再帰で求める
+// nをa以下の数の和でメモ化再帰で表す
 int partitionNumberRec(int n, int a) {
 	/*
 	基底条件:
 	a = 1 (1以下の自然数の和で表す = 全て1)のとき = それ以上分割できないとき= 1
 	n < 0 or a <= 0 (範囲外)のとき、0
 	*/
-	static int memo[ 100 ][ 100 ] = {};
+	static int memo[ ARR_SIZE ][ ARR_SIZE ] = {};
+	static int isRecorded[ ARR_SIZE ][ ARR_SIZE ] = {};
 
 	if (a == 1) {
-		// a = 1のとき、1以下の自然数の和で表す
-		// つまり、「全て1で表す」の１通りしか存在しない
+		/*
+		a = 1のとき、分割の方法は1つしかない
+		*/
 		memo[ n ][ a ] = 1;
+		isRecorded[ n ][ a ] = 1;
 		return (1);
 	} else if (n < 0 || a <= 0) {
-		// nそのものがマイナス、またはaがマイナス、またはaが0のとき、
-		// そもそも分割できないので、0
-		memo[ n ][ a ] = 0;
+		// nそのものが範囲外、もしくはaが0以下のとき、
+		// 分割できないということなので、0を返す
+		isRecorded[ n ][ a ] = 1;
 		return (0);
-	} else if (memo[ n ][ a ] == 0) {
-		/*
-		p(n,a)をnを自然数aで始まるように分割するときの分割数とすると、
-		p(n,a) = p(n-a,a) + p(n,a-1)といえる。
-		*/
-		memo[ n ][ a ] = partitionNumberRec(n - 1, a) + partitionNumberRec(n - 1, a - 1);
-	} else {
-		// すでに計算済みの値を使う
-		return (memo[ n ][ a ]);
+	} else if (isRecorded[ n ][ a ] == 0) {
+		// ここまで来たとき、まだメモ化されていない
+		// ここでメモ化する
+		memo[ n ][ a ] = partitionNumberRec(n - a, a) + partitionNumberRec(n, a - 1);
+		isRecorded[ n ][ a ] = 1;
 	}
+
+	return (memo[ n ][ a ]);
 }
