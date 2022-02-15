c1eda11484baaa2184ea9a16bc77e4a36f6f2920
hk-p114514 <hukada.gogotea@gmail.com>
2022-02-15 17:02:58 +0900

    starting hash

--- progi/ei2/kadai/hash/hash.c
+++ progi/ei2/kadai/hash/hash.c
@@ -1,12 +1,18 @@
 #include "hash.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 // 操作関数①   ：ハッシュテーブルの初期化（tbl[]の全要素のcountを0に、elementをNULLにする）
 //      tbl[]   ：ハッシュテーブル
 //      tbl_size：ハッシュテーブルのサイズ
 //      戻り値  ：なし
-void initHashTable(HashEntry tbl[], int tbl_size)
-{
+void initHashTable(HashEntry tbl[], int tbl_size) {
+	for (int i = 0; i < tbl_size; i++) {
+		tbl[ i ].count = 0;
+		tbl[ i ].element = NULL;
+	}
 
+	return;
 }
 
 // 操作関数②   ：キーと値からなる要素を動的に生成する
@@ -14,9 +20,9 @@ void initHashTable(HashEntry tbl[], int tbl_size)
 //      value   ：キーに対応する値
 //      戻り値  ：正常に生成できたとき     ＝ 生成した要素へのポインタ
 //	            ：メモリ確保に失敗したとき ＝ NULL
-HashElement *createElement(char *key, char *value)
-{
-
+HashElement *createElement(char *key, char *value) {
+	HashElement *elem = (HashElement *)malloc(sizeof(HashElement));
+	return (elem);
 }
 
 // 操作関数③   ：キーに対応する要素を検索する
@@ -24,9 +30,7 @@ HashElement *createElement(char *key, char *value)
 //      key     ：検索するキー
 //      戻り値  ：キーが見つかったとき ＝ キーに対応する要素へのポインタ
 //              ：キーが見つからなかったとき ＝ NULL
-HashElement *searchElement(HashEntry tbl[], char *key)
-{
-
+HashElement *searchElement(HashEntry tbl[], char *key) {
 }
 
 // 操作関数④   ：キーに対応する値のみを取得する
@@ -34,9 +38,7 @@ HashElement *searchElement(HashEntry tbl[], char *key)
 //      key     ：検索するキー
 //      戻り値  ：キーが見つかったとき       ＝ キーに対応する値（文字列）へのポインタ
 //              ：キーが見つからなかったとき ＝ NULL
-char *getValue(HashEntry tbl[], char *key)
-{
-
+char *getValue(HashEntry tbl[], char *key) {
 }
 
 // 操作関数⑤   ：キーとそれに対応する値からなる要素をハッシュテーブルに挿入する
@@ -47,9 +49,7 @@ char *getValue(HashEntry tbl[], char *key)
 //      戻り値  ：新たに要素を追加したとき               ＝ 1
 //              ：同一のキーと値を持つ要素が存在したとき ＝ 2
 //              ：メモリ確保に失敗したとき               ＝ 0
-int insertElement(HashEntry tbl[], char *key, char *value)
-{
-
+int insertElement(HashEntry tbl[], char *key, char *value) {
 }
 
 // 操作関数⑥   ：指定したキーがあればその値を更新し、なければ新たに要素を挿入する
@@ -58,9 +58,7 @@ int insertElement(HashEntry tbl[], char *key, char *value)
 //      value   ：キーに対応する値
 //      戻り値  ：更新または追加できたとき ＝ 1
 //              ：メモリ確保に失敗したとき ＝ 0
-int updateElement(HashEntry tbl[], char *key, char *value)
-{
-
+int updateElement(HashEntry tbl[], char *key, char *value) {
 }
 
 // 操作関数⑦   ：キーで要素を検索し、見つかった要素を削除する
@@ -68,46 +66,44 @@ int updateElement(HashEntry tbl[], char *key, char *value)
 //      key     ：検索するキー
 //      戻り値  ：キーが見つかり要素を削除したとき ＝ 1
 //              ：キーが見つからなかったとき       ＝ 0
-int removeElement(HashEntry tbl[], char *key)
-{
-
+int removeElement(HashEntry tbl[], char *key) {
 }
 
 // 操作関数⑧   ：ハッシュテーブルの要素をすべて削除する（メモリを解放する）
 //      tbl[]   ：ハッシュテーブル
 //      tbl_size：ハッシュテーブルのサイズ
 //      戻り値  ：なし
-void freeHashTable(HashEntry tbl[], int tbl_size)
-{
-
+void freeHashTable(HashEntry tbl[], int tbl_size) {
 }
 
 // 操作関数⑨   ：ハッシュテーブルのすべての要素をキーと値のペアで表示する
 //      tbl[]   ：ハッシュテーブル
 //      tbl_size：ハッシュテーブルのサイズ
-//i     戻り値  ：なし
-void printHashTable(HashEntry tbl[], int tbl_size)
-{
-
+// i     戻り値  ：なし
+void printHashTable(HashEntry tbl[], int tbl_size) {
 }
 
 // 操作関数⑩   ：キーからハッシュ値を求める
 //      tbl_size：ハッシュテーブルのサイズ
 //      戻り値  ：ハッシュ値（ハッシュテーブルのサイズを超えない非負整数）
 #ifndef MY_HASH
-unsigned int hash(unsigned char *key, int tbl_size)
-{
-  unsigned int hash_value;
+unsigned int hash(unsigned char *key, int tbl_size) {
+	unsigned int hash_value;
 
-  for (hash_value = 0; *key != '\0'; key++) {
-    hash_value = hash_value + (unsigned int)((*key) * (*key));
-  }
+	for (hash_value = 0; *key != '\0'; key++) {
+		hash_value = hash_value + (unsigned int)((*key) * (*key));
+	}
 
-  return(hash_value % tbl_size);
+	return (hash_value % tbl_size);
 }
 #else
-unsigned int hash(unsigned char *name, int tbl_size)
-{
-  // ここに、自分で考えたハッシュ関数を記述する
+unsigned int hash(unsigned char *name, int tbl_size) {
+	// ここに、自分で考えたハッシュ関数を記述する
 }
 #endif
+
+/*
+  概要:ハッシュテーブルのテーブルサイズを取得する
+*/
+// 第1引数: ハッシュテーブル
+// 返り値  : テーブルのサイズ
--- progi/ei2/kadai/hash/hash.h
+++ progi/ei2/kadai/hash/hash.h
@@ -1,28 +1,28 @@
-#ifndef TBL_SIZE              // もしTBL_SIZEが定義されていなければ
-  #define TBL_SIZE 2003       // TBL_SIZE(ハッシュテーブルサイズ)を2003にする
+#ifndef TBL_SIZE      // もしTBL_SIZEが定義されていなければ
+#define TBL_SIZE 2003 // TBL_SIZE(ハッシュテーブルサイズ)を2003にする
 #endif
 
-#define KEY_LEN 32            // キーの最大サイズ
-#define VAL_LEN 256           // 値の最大サイズ
+#define KEY_LEN 32  // キーの最大サイズ
+#define VAL_LEN 256 // 値の最大サイズ
 
-typedef struct hash_element {	// ハッシュテーブルに格納される一要素
-  char *key;                  // キー
-  char *value;                // キーに対応する値
-  struct hash_element *next;  // 次の要素(シノニム)へのポインタ
+typedef struct hash_element {  // ハッシュテーブルに格納される一要素
+	char *key;                 // キー
+	char *value;               // キーに対応する値
+	struct hash_element *next; // 次の要素(シノニム)へのポインタ
 } HashElement;
 
-typedef struct {              // ハッシュテーブルはこのデータ型の配列
-  int count;                  // このエントリを共有するシノニムの数
-  HashElement *element;       // 格納する要素(キーと値のペア)へのポインタ
+typedef struct {          // ハッシュテーブルはこのデータ型の配列
+	int count;            // このエントリを共有するシノニムの数
+	HashElement *element; // 格納する要素(キーと値のペア)へのポインタ
 } HashEntry;
 
-void initHashTable(HashEntry tbl[], int tbl_size);           // 操作関数①
-HashElement *createElement(char *key, char *value);          // 操作関数②
-HashElement *searchElement(HashEntry tbl[], char *key);	     // 操作関数③
-char *getValue(HashEntry tbl[], char *key);                  // 操作関数④
-int insertElement(HashEntry tbl[], char *key, char *value);  // 操作関数⑤
-int updateElement(HashEntry tbl[], char *key, char *value);  // 操作関数⑥
-int removeElement(HashEntry tbl[], char *key);               // 操作関数⑦
-void freeHashTable(HashEntry tbl[], int tbl_size);           // 操作関数⑧
-void printHashTable(HashEntry tbl[], int tbl_size);          // 操作関数⑨
-unsigned int hash(unsigned char *key, int tbl_size);         // 操作関数⑩
+void initHashTable(HashEntry tbl[], int tbl_size);          // 操作関数①
+HashElement *createElement(char *key, char *value);         // 操作関数②
+HashElement *searchElement(HashEntry tbl[], char *key);     // 操作関数③
+char *getValue(HashEntry tbl[], char *key);                 // 操作関数④
+int insertElement(HashEntry tbl[], char *key, char *value); // 操作関数⑤
+int updateElement(HashEntry tbl[], char *key, char *value); // 操作関数⑥
+int removeElement(HashEntry tbl[], char *key);              // 操作関数⑦
+void freeHashTable(HashEntry tbl[], int tbl_size);          // 操作関数⑧
+void printHashTable(HashEntry tbl[], int tbl_size);         // 操作関数⑨
+unsigned int hash(unsigned char *key, int tbl_size);        // 操作関数⑩
