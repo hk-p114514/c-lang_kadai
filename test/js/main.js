d64b1281b6f696a9226cce6d21bcaef9543980aa
hk-p114514 <hukada.gogotea@gmail.com>
2022-02-22 15:32:42 +0900

    delete waste file or directories

--- test/js/main.js
+++
@@ -1,7 +0,0 @@
-const rand = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-for (let i = 0; i < 100; i++) {
-  console.log(rand(rand(0, 100), rand(0, 100)));
-}
--- test/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  int n;
-
-  if ((n = getchar()) != EOF) {
-    printf("ok\n");
-  }
-}
